69bc8aacea63ebaa0d6a1a557a4ca15790166c71
Medad Newman <medadrufus@gmail.com>
2020-08-07 22:59:24 +0100

    Create enum test.py

---
+++ scratch_tests/enum test.py
@@ -0,0 +1,12 @@
+########################################
+#
+# Created: 07/08/2020
+# Author: Medad Newman
+#
+########################################
+
+
+import enum
+print(enum.__file__)
+# standard library location should be something like
+# /usr/local/lib/python3.6/enum.py
