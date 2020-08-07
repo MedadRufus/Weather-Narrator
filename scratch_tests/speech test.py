0cf307d270485b885f8456d699047a2950f503b4
Medad Newman <medadrufus@gmail.com>
2020-08-07 20:42:26 +0100

    Create speech test.py

---
+++ scratch_tests/speech test.py
@@ -0,0 +1,10 @@
+##########################################################
+#
+# Created: 07/08/2020
+# Author: Medad Newman
+#
+###########################################################
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("I will speak this text")
+engine.runAndWait()
